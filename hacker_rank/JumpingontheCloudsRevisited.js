e30aadec2ebe2578effbac8b77a31360cf3bff3c
soda-flavour <axa8380@gmail.com>
2020-08-29 15:02:17 +0900

    feat: Add Solution

---
+++ hacker_rank/JumpingontheCloudsRevisited.js
@@ -0,0 +1,13 @@
+function jumpingOnClouds(c, k) {
+  // k=2
+  let energy = 100;
+  let count = 0;
+  let index;
+  while (index !== 0 && energy >= 0) {
+    count === 0 && (index = 0);
+    index = (index + k) % c.length;
+    energy = c[index] > 0 ? energy - 3 : energy - 1;
+    count++;
+  }
+  return energy;
+}
